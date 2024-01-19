18e681a139d726f334f8a35c6975c218968404a5
M. James Harmon <mjamesharmon@users.noreply.github.com>
2024-01-19 18:22:23 +0000

    cleaning / refactoring QueryParametersBuilder

--- NewsAPI/Serialization/QueryParametersBuilder.cs
+++ NewsAPI/Serialization/QueryParametersBuilder.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Text;
 using NewsAPI.Attributes;
+using NewsAPI.Models;
 
 namespace NewsAPI.Serialization
 {
@@ -17,21 +18,25 @@ namespace NewsAPI.Serialization
 		public QueryParametersBuilder AppendParameter<T>(T request,
 			PropertyInfo property)
 		{
-			var attribute =
-				property.GetCustomAttribute<RequestParameterAttribute>() ??
-				null;
+			var applicator = GetApplicator<T>(property);
 
-			if (attribute != null) {
-				_values.Add(attribute.ApplyTo(request, property));
-			}
-	
+			_values.Add(applicator(request,property));
+
 			return this;
+		}
+
+		private Func<T, PropertyInfo, string> GetApplicator<T>(PropertyInfo property) {
+			RequestParameterAttribute? attribute =
+				property.GetCustomAttribute<RequestParameterAttribute>();
+
+			return (attribute == null) ? (d,p) => string.Empty :
+				attribute.ApplyTo;
 		}
 
         public override string ToString()
         {
 			return string.Join("&", _values.Where(v =>
-			string.IsNullOrWhiteSpace(v) == false));
+				string.IsNullOrWhiteSpace(v) == false));
         }
     }
 }
