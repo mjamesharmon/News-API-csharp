b158aa6dc2f018759c6509f6896cc9880f681d49
mjamesharmon <m.james.harmon@fastmail.com>
2024-01-29 22:31:28 -0800

    adding extensions to support easy querying and for dependency injection

---
+++ NewsAPI/Extensions/ClientExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+
+namespace NewsAPI.Extensions
+{
+	public static class ClientExtensions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="query"></param>
+		/// <param name="top"></param>
+		/// <returns></returns>
+		public static async Task<ArticlesResult> Query(
+			this NewsApiClient client,string query, int top=25) =>
+		 await client.GetEverythingAsync(
+				new EverythingRequest { Query = query, PageSize = top });
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="query"></param>
+		/// <param name="top"></param>
+		/// <returns></returns>
+        public static async Task<ArticlesResult> MostRecent(
+            this NewsApiClient client, string query, int top = 25) =>
+         await client.GetEverythingAsync(
+                new EverythingRequest {
+					Query = query,
+					PageSize = top,
+					SortBy = SortBys.PublishedAt});
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="query"></param>
+		/// <param name="top"></param>
+		/// <returns></returns>
+        public static async Task<ArticlesResult> MostPopular(
+           this NewsApiClient client, string query, int top = 25) =>
+        await client.GetEverythingAsync(
+               new EverythingRequest
+               {
+                   Query = query,
+                   PageSize = top,
+                   SortBy = SortBys.Popularity
+               });
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="query"></param>
+        /// <param name="top"></param>
+        /// <returns></returns>
+        public static async Task<ArticlesResult> TopHeadlines(
+			this NewsApiClient client, string query, int top=25) =>
+		 await client.GetTopHeadlinesAsync(
+				new TopHeadlinesRequest { Query = query, PageSize = top });
+		
+	}
+}
+
---
+++ NewsAPI/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using NewsAPI.Models;
+
+namespace NewsAPI.Extensions
+{
+	public static class DependencyInjectionExtensions
+	{
+
+		public static IServiceCollection AddNewsApiServices(
+			this IServiceCollection services,
+			Action<NewsApiClientOptions>? configuration = null)
+		
+		{
+			services.ConfigureNewsApiService(configuration);
+			services.AddHttpClient<NewsApiClient>();
+			services.AddTransient<NewsApiClient>();
+			return services;
+		}
+
+		private static IServiceCollection ConfigureNewsApiService(
+			this IServiceCollection services,
+			Action<NewsApiClientOptions>? configuration)
+		{
+			NewsApiClientOptions clientOptions = new();
+			configuration?.Invoke(clientOptions);
+			services.AddSingleton(clientOptions);
+			return services;
+		}
+	}
+}
+
